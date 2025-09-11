4fe8dd38174ab001926c57faa4660cae55821a1c
Antrika Kashyap <antrikakashyap2@gmail.com>
2025-09-12 00:27:29 +0530

    First commit

---
+++ Intro.java/Intro.java
@@ -0,0 +1,7 @@
+public class Intro{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        System.out.println("From today I will push some content on y github! ");
+        System.out.println("Today marks the day one of my github repo journey....");
+    }
+}
