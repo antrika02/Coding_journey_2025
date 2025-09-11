a4cac97e4c475a54324f120c8e56ddca0f1af3a1
Antrika Kashyap <antrikakashyap2@gmail.com>
2025-09-12 00:34:27 +0530

    Describe changes

--- Intro.java/Next.java
+++ Intro.java/Next.java
@@ -1,3 +1,9 @@
 public class Next {
-    
+    public static void main(String args[]){
+        int a, b;
+         a =10;
+         b= 10;
+        int sum = a+b;
+        System.out.println(sum);
+    }
 }
