aa1b1f9f3fcd694165fcd8730b5ef5150bf3fbd5
Antrika Kashyap <antrikakashyap2@gmail.com>
2025-09-16 19:47:02 +0530

    Add all existing local code

--- Intro.java/Backtracking.java
+++ Intro.java/Backtracking.java
@@ -13,10 +13,39 @@ public class Backtracking {
         }
         System.out.println();
     }
-    public static void main(String args[]){
-        int arr[] = new int[10];
-        firstProgram(arr , 20, 0 );
-        printArr(arr);
+    // public static void main(String args[]){
+    //     int arr[] = new int[10];
+    //     firstProgram(arr , 20, 0 );
+    //     printArr(arr);
+    // }
+
+
+    public static void findSubsets(String str, String ans, int i){
+        if(i == string.lenght()){
+            System.out.println(ans);
+            return;
+        }
+        findSubsets(str, ans+str.charAt(i), i+1);
+        findSubsets(str, ans, i+1);
     }
+    // public static void main(String args[]){
+    //     String str = new String[5];
+    //     findSubstring(str, a, 0);
+    // }
+
+
+    public static void permutation(String str, String ans){
+        if(i == str.length()){
+            System.out.println(and);
+            return;
+        }
+        for(a = 0, a <= str.lenght() ; a++){
+            char curr = str.charAt(i);
+            String newString = subString(0,i) + str.String(i+"1");
+            find 
+        }
+    }
+
+
     
 }
