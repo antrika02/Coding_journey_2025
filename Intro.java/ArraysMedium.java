2231f95f566ec67e3f487d0836d92ecf0c2b4f15
Antrika Kashyap <antrikakashyap2@gmail.com>
2025-09-23 22:51:50 +0530

    Add all existing local code

--- Intro.java/ArraysMedium.java
+++ Intro.java/ArraysMedium.java
@@ -1,5 +1,58 @@
+import java.util.*;
 public class ArraysMedium {
-    public static void main(String args[]){
-        
+    public static int majorityElement(int []v) {
+   
+        int n = v.length;
+        int cnt = 0; 
+        int el = 0; 
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = v[i];
+            } else if (el == v[i]) cnt++;
+            else cnt--;
+        }
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (v[i] == el) cnt1++;
+        }
+
+        if (cnt1 > (n / 2)) return el;
+        return -1;
     }
+
+    // public static void main(String args[]) {
+    //     int[] arr = {2, 2, 1, 1, 1, 2, 2};
+    //     int ans = majorityElement(arr);
+    //     System.out.println("The majority element is: " + ans);
+
+    // }
+
+     public static long maxSubarraySum(int[] arr, int n) {
+        long maxi = Long.MIN_VALUE; // maximum sum
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += arr[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return maxi;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int n = arr.length;
+        long maxSum = maxSubarraySum(arr, n);
+        System.out.println("The maximum subarray sum is: " + maxSum);
+
+    }
+
 }
